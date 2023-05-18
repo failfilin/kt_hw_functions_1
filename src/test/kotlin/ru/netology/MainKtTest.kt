eb10520173f2cd8a54f360054dae3af48e4c5c15
Failfilin <apseliverstov@yandex.ru>
2023-05-18 18:36:11 +0300

    Commit with correct test

--- src/test/kotlin/ru/netology/MainKtTest.kt
+++ src/test/kotlin/ru/netology/MainKtTest.kt
@@ -1,3 +1,6 @@
+package ru.netology
+
+import commisionAmount
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -9,9 +12,7 @@ class MainKtTest {
         val cardType = "Maestro" // тип карты/счета при переводе
         val alreadyPayAmmount = 600_000 //сумма переводов в это месяце
         val payAmmount = 1500 // размер платежа
-
-        val result = commisionAmount(cardType,alreadyPayAmmount,payAmmount)
-
-        assertEquals(90,result)
+        val result = commisionAmount(cardType, alreadyPayAmmount, payAmmount)
+        assertEquals(29,result.toInt())
     }
 }
