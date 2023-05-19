a16afcf3bf3710f28dad838ae372b5e33b6b4da6
Failfilin <apseliverstov@yandex.ru>
2023-05-19 12:50:49 +0300

    Commit with full test on calc function

--- src/test/kotlin/ru/netology/MainKtTest.kt
+++ src/test/kotlin/ru/netology/MainKtTest.kt
@@ -8,11 +8,53 @@ import org.junit.Assert.*
 class MainKtTest {
 
     @Test
-    fun commisionAmount() {
+    fun commisionAmountMaestroMasterOverALreadyLimit() { // лимит уже превышен
         val cardType = "Maestro" // тип карты/счета при переводе
         val alreadyPayAmmount = 600_000 //сумма переводов в это месяце
         val payAmmount = 1500 // размер платежа
         val result = commisionAmount(cardType, alreadyPayAmmount, payAmmount)
-        assertEquals(29,result.toInt())
+        assertEquals(29, result.toInt())
+    }
+    @Test
+    fun commisionAmountMaestroMasterOverLimit() {// пересекается лимит при новом платеже
+        val cardType = "Maestro" // тип карты/счета при переводе
+        val alreadyPayAmmount = 74_000 //сумма переводов в это месяце
+        val payAmmount = 2900 // размер платежа
+        val result = commisionAmount(cardType, alreadyPayAmmount, payAmmount)
+        assertEquals(31, result.toInt())
+    }
+    @Test
+    fun commisionAmountMaestroMasterUnderLimit() {// пересекается лимит при новом платеже
+        val cardType = "Maestro" // тип карты/счета при переводе
+        val alreadyPayAmmount = 60_000 //сумма переводов в это месяце
+        val payAmmount = 2900 // размер платежа
+        val result = commisionAmount(cardType, alreadyPayAmmount, payAmmount)
+        assertEquals(0, result.toInt())
+    }
+
+    @Test
+    fun commisionAmountVisaMirOverMinCommision() {
+        val cardType = "Visa" // тип карты/счета при переводе
+        val alreadyPayAmmount = 6_000 //сумма переводов в это месяце
+        val payAmmount = 6000 // размер платежа
+        val result = commisionAmount(cardType, alreadyPayAmmount, payAmmount)
+        assertEquals(45, result.toInt())
+    }
+    @Test
+    fun commisionAmountVisaMirUnderMinCommision() {
+        val cardType = "Visa" // тип карты/счета при переводе
+        val alreadyPayAmmount = 6_000 //сумма переводов в это месяце
+        val payAmmount = 1500 // размер платежа
+        val result = commisionAmount(cardType, alreadyPayAmmount, payAmmount)
+        assertEquals(35, result.toInt())
+    }
+
+    @Test
+    fun commisionAmountElse() {
+        val cardType = "SomeElse" // тип карты/счета при переводе
+        val alreadyPayAmmount = 6_000 //сумма переводов в это месяце
+        val payAmmount = 1500 // размер платежа
+        val result = commisionAmount(cardType, alreadyPayAmmount, payAmmount)
+        assertEquals(0, result.toInt())
     }
 }
